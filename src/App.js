d32bf3db2f90165485fdec39aa63ceb190bbc5b0
Kyle Hunter <kyle.hunter91@gmail.com>
2022-12-16 05:43:11 -0800

    exercise 4: solution without z-index Relies on DOM ordering instead

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import React from "react";
 import CharacterEditor from "./components/CharacterEditor";
 import Footer from "./components/Footer";
 
-import styles from "./components/CharacterEditor/CharacterEditor.module.css";
-
 function App() {
   return (
     <>
       <CharacterEditor />
-      <div className={styles.perspectiveDecoration} />
       <Footer />
     </>
   );
