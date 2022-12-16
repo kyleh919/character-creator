c1a2023020e6f7afbb58eb488830548e3e94f06d
Kyle Hunter <kyle.hunter91@gmail.com>
2022-12-16 05:24:31 -0800

    exercise 4: solution with z-index

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+
+import styles from "./components/CharacterEditor/CharacterEditor.module.css";
 
 function App() {
   return (
     <>
       <CharacterEditor />
+      <div className={styles.perspectiveDecoration} />
       <Footer />
     </>
   );
